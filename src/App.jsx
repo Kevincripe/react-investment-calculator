83e4ecc582bd8cdd4db32cf6eec3babfb291a5b1
Kevin Cripe <kevincripe6@gmail.com>
2024-08-18 15:42:06 -0500

    add input validation

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,8 @@ function App() {
     duration: 10
   });
 
+  const inputIsValid = userInput.duration >= 1;
+
   function handleChange(inputIdentifier, newValue) {
     setUserInput(prevUserInput => {
         return {
@@ -24,11 +26,13 @@ function App() {
     });
 }
 
+
   return (
     <>
     <Header />
     <UserInput userInput={userInput} onChange={handleChange} />
-    <Results input={userInput} />
+    {!inputIsValid && <p className="center">Please Enter a Duration greater than 0.</p> }
+    {inputIsValid && <Results input={userInput} />}
     </>
   );
 }
